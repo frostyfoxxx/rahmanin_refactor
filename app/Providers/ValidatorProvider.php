98113d4f46d16b0474de2025d7eac58d67d2f18c
SvMa <svetakrivaya2002@gmail.com>
2021-09-30 15:36:21 +0400

    validator

--- app/Providers/ValidatorProvider.php
+++ app/Providers/ValidatorProvider.php
@@ -9,23 +9,29 @@ class ValidatorProvider extends ServiceProvider
 {
     // TODO: Поле 'phone_number' нуждается в правиле  'unique:users,phone_number' для регистрации, но не нуждается для авторизации
     private static $rules = [
-        'phone_number' => ['required', 'string', 'max:11'],
-        'email' => ['required', 'string', 'unique:users,email'],
+        'phone_number' => ['required', 'numeric', 'digits:11'],
+        'email' => ['required', 'email', 'unique:users,email'],
         'password' => ['required'],
         'first_name' => ['required', 'string'],
         'last_name' => ['required', 'string'],
-        'phone' => ['required', 'string'],
+        'phone' => ['required', 'numeric', 'digits:11'],
         'document_number' => ['required', 'string', 'max:10'],
-        'password' => ['required', 'string'],
+        'password' => ['required', 'string', 'min:6'],
     ];
 
-    public static function globalValidation($req)
+    public static function globalValidation($req, $options = null)
     {
         // array_keys(%)
         $fields = [];
 
         foreach (self::$rules as $key => $value) {
             if (array_key_exists($key, $req)) {
+                $fields[$key] = $value;
+            }
+        }
+
+        if ($options) {
+            foreach ($options as $key => $value) {
                 $fields[$key] = $value;
             }
         }
