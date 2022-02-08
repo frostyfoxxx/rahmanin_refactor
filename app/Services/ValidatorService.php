84581c116dd9cd1253dc1e2557ee3e36d0bc24df
Nail <frosty.fox14@yandex.ru>
2022-02-08 14:08:19 +0400

    fix validator: added $fields parameter

--- app/Services/ValidatorService.php
+++ app/Services/ValidatorService.php
@@ -9,13 +9,16 @@ class ValidatorService
 {
 
     private static $rules = [
+        // Регистрация и Авторизация
         'phone_number' => ['required', 'numeric', 'digits:11'],
         'email' => ['required', 'email', 'unique:users,email'],
+        'password' => ['required', 'string', 'min:6'],
+        // Персональные данные
+        'phone' => ['required', 'numeric', 'digits:11'],
         'first_name' => ['required', 'string'],
         'last_name' => ['required', 'string'],
-        'phone' => ['required', 'numeric', 'digits:11'],
-        'document_number' => ['required', 'string', 'max:10'],
-        'password' => ['required', 'string', 'min:6'],
+        'middle_name' => ['required', 'string'],
+        // Паспортные данные
         'series' => 'required|string|digits:4',
         'number' => 'required|string|digits:6',
         'date_of_issue' => 'required|date_format:Y-m-d',
@@ -25,36 +28,29 @@ class ValidatorService
         'place_of_birth' => 'required|string|max:255',
         'registration_address' => 'required|string|max:255',
         'lack_of_citizenship' => 'required|boolean',
+        // Данные о школе
         'school_name' => 'required|string|max:255',
         'number_of_classes' => 'required|integer',
         'year_of_ending' => 'required|date_format:Y',
         'number_of_certificate' => 'required|numeric|digits:14',
+        // Школьные предметы
         "*.subject" => ['required', 'string'],
         '*.appraisal' => ['required', 'numeric', 'between:3,5']
     ];
 
     /**
      * @param Request $req - Объект с приходящими данными
-     * @param array $options - массив с доп.полями для проверок
+     * @param array $fields - массив с полями
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    public function globalValidation(Request $req, array $options = [])
+    public function globalValidation(Request $req, array $fields)
     {
-        $fields = [];
-
         foreach (self::$rules as $key => $value) {
-            if (array_key_exists($key, $req)) {
-                $fields[$key] = $value;
+            if (in_array($key, $fields)) {
+                $validatorRules[$key] = $value;
             }
         }
-
-        if (!empty($options)) {
-            foreach ($options as $key => $value) {
-                $fields[$key] = $value;
-            }
-        }
-
-        return Validator::make($req->all(), $fields);
+        return Validator::make($req->all(), $validatorRules);
     }
 
 }
