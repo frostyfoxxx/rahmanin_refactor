adbfa1175abb0fa417e71f96738ac430384695b7
ookunokao <frosty.fox1337@gmail.com>
2023-12-30 17:15:29 +0400

    [refactor]: Небольшой рефакторинг кода регистрации и персональных данных

--- app/Http/Controllers/PersonalDataController.php
+++ app/Http/Controllers/PersonalDataController.php
@@ -91,7 +91,7 @@ class PersonalDataController extends Controller
         $foundedData = Auth::user()->personalData ? Auth::user()->personalData->all() : [];
         return response()->json([
             'code' => Response::HTTP_CREATED,
-            'message' => 'Personal Data found',
+            'message' => 'Personal Data founded',
             'data' => PersonalsDataResource::collection($foundedData)
         ])->setStatusCode(Response::HTTP_CREATED);
     }
